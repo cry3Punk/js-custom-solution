e1cb15b9a24d83f2619c564359f37a66f6e177e1
cry3Punk <163534326+cry3Punk@users.noreply.github.com>
2024-03-25 16:19:30 +0200

    Check if Object Instance of Class

--- tasks.js
+++ tasks.js
@@ -1,4 +1,4 @@
-//✅ Came back after almost 1 year of break
+//1. ✅ Came back after almost 1 year of break
 //Took - 40min
 const doStuff = (str, k) => {
     const arr = [[]];
@@ -33,3 +33,32 @@ const sumAssesment = (str, k) => {
 
    return doStuff(str, k)
 }
+
+//2. ✅ Check if Object Instance of Class
+//My dumb solution which partially works
+const checkIfInstanceOf = function(obj, classFunction) {
+    let startingPoint = 'obj.__proto__';
+
+    while(eval(startingPoint).constructor.name !== 'Object'){
+        if(eval(startingPoint).constructor.name === classFunction.name) return true;
+
+        startingPoint += '.__proto__'
+    }
+
+    return false;
+};
+
+//Why do we need to check proto if we'll just have same prototype for both, right?
+//better approach:
+const checkIfInstanceOf = function(obj, classFunction) {
+    while(obj!=null){
+        if(obj.constructor === classFunction)
+            return true;
+
+        obj = Object.getPrototypeOf(obj);
+    }
+
+    return false;
+};
+
+//3. 
