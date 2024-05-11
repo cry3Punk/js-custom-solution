7c2bc5e8202e0a67820ff57316cb0b8c737d2942
cry3Punk <163534326+cry3Punk@users.noreply.github.com>
2024-05-11 18:22:14 +0300

    custom reduce

--- tasks.js
+++ tasks.js
@@ -95,3 +95,23 @@ function palindrome(string) {
   
   return isPalindrome;
 }
+
+//custom reduce
+
+const a = [10, 21, 13, 56];
+
+function add(a, b) { return a + b }
+function foo(a, b) { return a.concat(b) }
+
+Array.prototype.reduce2 = function (f, result) {
+  const i = 0;
+  if (arguments.length < 2) {
+    i = 1;
+    result = this[0];
+  }
+    
+  for(; i < this.length; i++)
+    result = f(result, this[i], i, this);
+    
+  return result;
+};
