3ebd00edd5392b3bf5db930c6332c3209646e2ba
cry3Punk <163534326+cry3Punk@users.noreply.github.com>
2024-05-14 10:19:18 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -131,3 +131,23 @@ function curry(fun,...args) {
     return fun.call(this,...args,...arg2);
   }
 }
+
+//baby magpies
+const child = function(bird1, bird2) {
+  if (bird1===bird2) return false
+  return ( getDifference(bird1, bird2) < 3 ? true : false );
+}
+
+const grandchild = function(bird1, bird2) {
+  if (bird1.length===1&&bird2.length===1&&bird2!==bird1) return false
+  return ( getDifference(bird1, bird2) <=4 ? true : false );
+}
+
+function getDifference(bird1,bird2) {
+        let dif = 0;
+    
+        for(let i=0; i<bird1.length; i++)
+          if(bird1.slice(i,i+1)!==bird2.slice(i,i+1)) dif++;
+    
+        return dif;
+}
