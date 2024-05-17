28dffb2993cb270f048344839265238beba08cb4
cry3Punk <163534326+cry3Punk@users.noreply.github.com>
2024-05-17 11:35:53 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -151,3 +151,9 @@ function getDifference(bird1,bird2) {
     
         return dif;
 }
+
+//bowling pins
+const bowlingPins = function(arr){
+  return "7 8 9 10\n 4 5 6 \n  2 3  \n   1   ".split`\n`.map(
+  v=>v.split` `.map(v=>arr.includes(v*1)?' ':v===''?'':'I').join` `).join`\n`
+}
