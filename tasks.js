aec9fe5446025ffc962c43f9e008a1a4a92a3b77
cry3Punk <163534326+cry3Punk@users.noreply.github.com>
2024-05-19 21:38:37 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -157,3 +157,23 @@ const bowlingPins = function(arr){
   return "7 8 9 10\n 4 5 6 \n  2 3  \n   1   ".split`\n`.map(
   v=>v.split` `.map(v=>arr.includes(v*1)?' ':v===''?'':'I').join` `).join`\n`
 }
+
+//array helpers
+Array.prototype.square=function(){
+return this.map(v=>v*v)
+}
+Array.prototype.cube=function(){
+return this.map(v=>v*v*v)
+}
+Array.prototype.sum=function(){
+return this.reduce((a,b)=>a+b,0)
+}
+Array.prototype.average=function(){
+return this.reduce((a,b)=>a+b,0)/this.length
+}
+Array.prototype.even=function(){
+return this.filter(v=>v%2==0)
+}
+Array.prototype.odd=function(){
+return this.filter(v=>v%2!==0)
+}
